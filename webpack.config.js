4ffca88f713a118ab766db9e1ce7d733dfb5b41f
akoarum <kamome-black@akoarum.net>
2018-01-05 14:01:05 +0900

    環境設定ファイル関連のアップデート

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,59 @@ const path = require('path');
 module.exports = [
   {
     entry: {
+      options: './dev/js/options/index.js'
+    },
+    output: {
+      path: path.join(__dirname, './app/js'),
+      filename: '[name].js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015', 'stage-0']
+          }
+        },
+        {
+          test: /\.vue$/,
+          exclude: /node_modules/,
+          loader: 'vue-loader'
+        }
+      ]
+    },
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js',
+        'vuex$': 'vuex/dist/vuex.js'
+      }
+    }
+  },
+  {
+    entry: {
+      background: './dev/js/background/index.js'
+    },
+    output: {
+      path: path.join(__dirname, './app/js'),
+      filename: '[name].js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015', 'stage-0']
+          }
+        }
+      ]
+    }
+  },
+  {
+    entry: {
       options: './dev/scss/options.scss'
     },
     output: {
