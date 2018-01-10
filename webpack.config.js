673842b774e6dbdeff9932fc6c85f0fedab26e3a
akoarum <kamome-black@akoarum.net>
2018-01-10 11:56:01 +0900

    リリース用のタスクを追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,89 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
+const RELEASE = /release/.test(process.env.npm_lifecycle_event);
 
-module.exports = [
-  {
-    entry: {
-      options: './dev/js/options/index.js'
-    },
-    output: {
-      path: path.join(__dirname, './app/js'),
-      filename: '[name].js'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          query: {
-            presets: ['es2015', 'stage-0']
-          }
-        },
-        {
-          test: /\.vue$/,
-          exclude: /node_modules/,
-          loader: 'vue-loader'
+/**
+ * JSのビルド
+ */
+const js = {
+  entry: {
+    options: './dev/js/options/index.js'
+  },
+  output: {
+    path: path.join(__dirname, './app/js'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0']
         }
-      ]
-    },
-    resolve: {
-      alias: {
-        'vue$': 'vue/dist/vue.esm.js',
-        'vuex$': 'vuex/dist/vuex.js'
+      },
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: 'vue-loader'
       }
-    }
-  },
-  {
-    entry: {
-      background: './dev/js/background/index.js'
-    },
-    output: {
-      path: path.join(__dirname, './app/js'),
-      filename: '[name].js'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          query: {
-            presets: ['es2015', 'stage-0']
-          }
-        }
-      ]
-    }
-  },
-  {
-    entry: {
-      options: './dev/scss/options.scss'
-    },
-    output: {
-      path: path.join(__dirname, './app/css/'),
-      filename: '[name].css'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.scss$/,
-          loader: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              'css-loader?minimize',
-              'postcss-loader',
-              'sass-loader'
-            ]
-          })
-        }
-      ]
-    },
-    plugins: [
-      new ExtractTextPlugin('[name].css')
     ]
+  },
+  resolve: {
+    alias: {
+      'vue$': (RELEASE) ? 'vue/dist/vue.min.js' : 'vue/dist/vue.esm.js',
+      'vuex$': 'vuex/dist/vuex.js'
+    }
   }
-];
+};
+
+
+/**
+ * CSSのコンパイル
+ */
+const css = {
+  entry: {
+    options: './dev/scss/options.scss'
+  },
+  output: {
+    path: path.join(__dirname, './app/css/'),
+    filename: '[name].css'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader?minimize',
+            'postcss-loader',
+            'sass-loader'
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ]
+};
+
+
+/**
+ * リリース
+ */
+if (RELEASE) {
+  js.plugins = [
+    new webpack.DefinePlugin({
+      'process_env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ];
+}
+
+module.exports = [ js, css ];
